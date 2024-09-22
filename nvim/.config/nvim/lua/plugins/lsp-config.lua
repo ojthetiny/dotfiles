6b4731c83b750bb2bf12e176770f88c9963d35d5
Liv <ba6sti6@gmail.com>
2024-09-22 21:05:57 +0300

    Add completions and autoformatting

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,33 +1,44 @@
 return {
-   {
-      "williamboman/mason.nvim",
-      config = function()
-         require ("mason").setup()
-      end
-   },   
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-   {
-      "williamboman/mason-lspconfig",
-      config = function()
-         require("mason-lspconfig").setup ({
-            ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "pylsp", "cmake", "jsonls" }   
-         })
-      end
-   },
+	{
+		"williamboman/mason-lspconfig",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "pylsp", "cmake" },
+			})
+		end,
+	},
 
-   {
-      "neovim/nvim-lspconfig",
-      config = function()
-         local lspconfig = require("lspconfig")
-         lspconfig.lua_ls.setup({})
-         lspconfig.rust_analyzer.setup({})
-         lspconfig.clangd.setup({})
-         lspconfig.cmake.setup({})
-         lspconfig.pylsp.setup({})
-         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-         vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
-      end
-   }
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cmake.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pylsp.setup({
+				capabilities = capabilities,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
-
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "lua", "c", "python", "rust", "javascript" },
+      auto_install = true,
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
